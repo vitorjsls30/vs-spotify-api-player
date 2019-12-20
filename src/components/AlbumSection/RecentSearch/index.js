7247f3f1f86d30c3d4991fe0256221282689dcc1
Vitor José de Souza Lopes Sacramento <vitor.sacramento@MIDGARD.LOCAL>
2019-12-20 13:57:49 -0300

    swp-02 - Extracting Recent Search and Search Result List into 2 new components

---
+++ src/components/AlbumSection/RecentSearch/index.js
@@ -0,0 +1,58 @@
+import React from 'react';
+
+const RecentSearch = () => {
+  return(
+    <div className="items-block">
+        <p className="section-title">&Aacute;lbuns buscados recentemente</p>
+        <div className="album-container">
+          <div>
+            <img className="album-cover" />
+            <p className="album-name">Nome do &aacute;lbum</p>
+            <p className="artist-name">Nome do artista</p>
+          </div>
+          <div>
+            <img className="album-cover" />
+            <p className="album-name">Nome do &aacute;lbum</p>
+            <p className="artist-name">Nome do artista</p>
+          </div>
+          <div>
+            <img className="album-cover" />
+            <p className="album-name">Nome do &aacute;lbum</p>
+            <p className="artist-name">Nome do artista</p>
+          </div>
+          <div>
+            <img className="album-cover" />
+            <p className="album-name">Nome do &aacute;lbum</p>
+            <p className="artist-name">Nome do artista</p>
+          </div>
+          <div>
+            <img className="album-cover" />
+            <p className="album-name">Nome do &aacute;lbum</p>
+            <p className="artist-name">Nome do artista</p>
+          </div>
+          <div>
+            <img className="album-cover" />
+            <p className="album-name">Nome do &aacute;lbum</p>
+            <p className="artist-name">Nome do artista</p>
+          </div>
+          <div>
+            <img className="album-cover" />
+            <p className="album-name">Nome do &aacute;lbum</p>
+            <p className="artist-name">Nome do artista</p>
+          </div>
+          <div>
+            <img className="album-cover" />
+            <p className="album-name">Nome do &aacute;lbum</p>
+            <p className="artist-name">Nome do artista</p>
+          </div>
+          <div>
+            <img className="album-cover" />
+            <p className="album-name">Nome do &aacute;lbum</p>
+            <p className="artist-name">Nome do artista</p>
+          </div>
+        </div>
+      </div>
+  );
+};
+
+export default RecentSearch;
---
+++ src/components/AlbumSection/SearchResult/index.js
@@ -0,0 +1,76 @@
+import React from 'react';
+
+const SearchResult = () => {
+  return (
+    <div className="items-block">
+        <p className="section-title">Resultados para "Busca"</p>
+        <div className="search-result">
+          <div className="search-item">
+            <img className="album-cover" />
+            <div className="album-detail">
+              <p className="album-name">Nome do &aacute;lbum</p>
+              <p className="artist-name">Nome do artista</p>
+            </div>
+          </div>
+          <div className="search-item">
+            <img className="album-cover" />
+            <div className="album-detail">
+              <p className="album-name">Nome do &aacute;lbum</p>
+              <p className="artist-name">Nome do artista</p>
+            </div>
+          </div>
+          <div className="search-item">
+            <img className="album-cover" />
+            <div className="album-detail">
+              <p className="album-name">Nome do &aacute;lbum</p>
+              <p className="artist-name">Nome do artista</p>
+            </div>
+          </div>
+          <div className="search-item">
+            <img className="album-cover" />
+            <div className="album-detail">
+              <p className="album-name">Nome do &aacute;lbum</p>
+              <p className="artist-name">Nome do artista</p>
+            </div>
+          </div>
+          <div className="search-item">
+            <img className="album-cover" />
+            <div className="album-detail">
+              <p className="album-name">Nome do &aacute;lbum</p>
+              <p className="artist-name">Nome do artista</p>
+            </div>
+          </div>
+          <div className="search-item">
+            <img className="album-cover" />
+            <div className="album-detail">
+              <p className="album-name">Nome do &aacute;lbum</p>
+              <p className="artist-name">Nome do artista</p>
+            </div>
+          </div>
+          <div className="search-item">
+            <img className="album-cover" />
+            <div className="album-detail">
+              <p className="album-name">Nome do &aacute;lbum</p>
+              <p className="artist-name">Nome do artista</p>
+            </div>
+          </div>
+          <div className="search-item">
+            <img className="album-cover" />
+            <div className="album-detail">
+              <p className="album-name">Nome do &aacute;lbum</p>
+              <p className="artist-name">Nome do artista</p>
+            </div>
+          </div>
+          <div className="search-item">
+            <img className="album-cover" />
+            <div className="album-detail">
+              <p className="album-name">Nome do &aacute;lbum</p>
+              <p className="artist-name">Nome do artista</p>
+            </div>
+          </div>
+        </div>
+      </div>
+  );
+};
+
+export default SearchResult;
