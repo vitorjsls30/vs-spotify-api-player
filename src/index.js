b38111327f85e32811bfaede141879358d401867
Vitor José <vitorjsls30@gmail.com>
2020-01-25 21:07:46 -0300

    swp-03 - Adjusting SearchBar component and App Component to filter our searched terms

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SideBar from './components/SideBar';
 import SearchBar from './components/SearchBar';
@@ -6,9 +6,34 @@ import AlbumSection from './components/AlbumSection';
 
 import './style.css';
 
-const wrapper = document.getElementById('root');
-wrapper ? ReactDOM.render(<div>
-  <SideBar />
-  <SearchBar />
-  <AlbumSection />
-</div>, wrapper) : false;
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      search: {
+        term: ''
+      }
+    }
+  }
+
+  onSearchTermChange(term) {
+    // We use the spotify-wrapper to search for the given term
+    // We then set the returned search results into the App state
+    this.setState({ search: { term } }, () => {
+        console.log('State setted...', this.state);
+      });
+    // Album section renders any given changes in the search result
+  }
+
+  render() {
+    return (<div>
+      <SideBar />
+      <SearchBar onSearchTermChange={this.onSearchTermChange.bind(this)} />
+      <AlbumSection />
+    </div>);
+  }
+}
+
+const root = document.getElementById('root');
+root ? ReactDOM.render(<App />, root) : false;
