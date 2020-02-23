96c8e61f044d890ee0818b554563ab54b51349bf
vitor José <vitorjsls30@gmail.com>
2020-02-23 16:30:12 -0300

    swp-04 - Adding AlbumInfo component into the Detail page

---
+++ src/components/AlbumSection/Detail/AlbumInfo.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function AlbumInfo(props) {
+  if (!props.albumInfo) {
+    return (
+      <div className="album-info">
+        <div className="album-name">Ops! No Album was found. Did you forget something? ;)</div>
+      </div>
+    );
+  }
+  let { cover, name, artist } = props.albumInfo;
+
+  return (
+    <div className="album-info">
+      <div className="detail-cover">
+        <img src={cover} width="200" height="200"/>
+      </div>
+      <div className="album-name">{ name }</div>
+      <div className="artist-name">{ artist }</div>
+    </div>
+  );
+}
+
+export default AlbumInfo;
--- src/components/AlbumSection/Detail/index.js
+++ src/components/AlbumSection/Detail/index.js
@@ -1,19 +1,73 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+
+import ALbumInfo from './AlbumInfo';
 import spotifyWrapper from '../../../services';
+import AlbumInfo from './AlbumInfo';
 
 class Detail extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      spotiFyWrapper: spotifyWrapper()
+      spotiFyWrapper: spotifyWrapper(),
+      albumInfo: {
+        cover: null,
+        name: null,
+        artist: null,
+        tracks: []
+      },
+      error: { status: null, message: null }
+    }
+  }
+
+  getAlbumInfo() {
+    let { albumId } = this.props.match.params || null;
+
+    if (!albumId) {
+      this.setState((state) => {
+        return {
+          ...state,
+          error: { status: 500, message: 'No AlbumId parameter' }
+        }
+      }, () => { console.log('NEW STATE', this.state) });
+    }
+
+    if (albumId) {
+      this.state.spotiFyWrapper.album.getAlbum(albumId)
+        .then(album => {
+          console.log('Info', album);
+          let { error } = album;
+          if (error) {
+            this.setState((state) => {
+              return { ...state, error }
+            }, () => { console.log('ERROR AT GETALBUM - NEW STATE', this.state) });
+
+            return;
+          }
+          console.log('BEFORE SETSTATE');
+          this.setState(state => {
+            return {
+              ...state,
+              albumInfo: {
+                cover: album.images[1].url || null,
+                name: album.name,
+                artist: album.artists[0].name,
+                tracks: []
+              }
+            }
+          }, () => {
+            console.log('AFTER GETALBUM', this.state.albumInfo);
+          });
+        });
     }
   }
 
   componentDidMount() {
     // TODO: Now we use the spotifyWrapper and get the ALbumInfo...
     console.log(`AlbumId: ${this.props.match.params.albumId}`);
+    this.getAlbumInfo();
   }
+
   render() {
     return (
       <div>
@@ -28,11 +82,7 @@ class Detail extends Component {
         </section>
         <section className="section-detail">
           <section className="section-album-info">
-            <div className="album-info">
-              <div className="detail-cover"></div>
-              <div className="album-name">Album Name</div>
-              <div className="artist-name">Artist Name</div>
-            </div>
+            { <AlbumInfo albumInfo={this.state.albumInfo} />}
           </section>
           <section className="section-album-tracks">
             <div className="album-tracks">
