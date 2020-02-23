07f68b6a94088ea3c160dc1655330d48ea523186
vitor José <vitorjsls30@gmail.com>
2020-02-22 21:51:20 -0300

    swp-04 - Adjusting Link component usage in the SearchItem class to avoid styling change on it

--- src/components/AlbumSection/SearchResult/SearchItem.js
+++ src/components/AlbumSection/SearchResult/SearchItem.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SearchItem = (props) => {
   const { name, artist, images, id } = props.album;
   const cover = images.length ? images[1].url : '';
   return (
     <div className="search-item">
-      <img className="album-cover" src={cover} />
-      <div className="album-detail">
-        <p className="album-name">{name}</p>
-        <p className="artist-name">{artist}</p>
-      </div>
+      <Link to={`/${id}`} style={{textDecoration: 'none'}}>
+        <img className="album-cover" src={cover} />
+        <div className="album-detail">
+          <p className="album-name">{name}</p>
+          <p className="artist-name">{artist}</p>
+        </div>
+      </Link>
     </div>
   );
 };
