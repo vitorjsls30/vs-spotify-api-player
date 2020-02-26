d894db1ff80c264d8150d8c5512f3570c024e6c6
vitor José <vitorjsls30@gmail.com>
2020-02-26 15:40:03 -0300

    swp-05 - Adjusting nopm scripts to run at heroku

--- webpack.common.js
+++ webpack.common.js
@@ -7,7 +7,7 @@ module.exports = {
         app: './src/index.js',
     },
     output: {
-        path: path.join(__dirname, 'dist'),
+        path: path.join(__dirname, 'build'),
         publicPath: '/'
     },
     plugins: [
