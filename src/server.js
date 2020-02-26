c74c858eb56207d155cf504d47821192175b6eb8
vitor José <vitorjsls30@gmail.com>
2020-02-26 18:11:22 -0300

    swp-05 - Adding Procfile / changing redirectUri for the staging environment for tests

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const PORT = process.env.PORT || 9000
 
 app.use(express.static(path.join(__dirname, '..', 'build')));
 
@@ -8,4 +9,4 @@ app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
 });
 
-app.listen(9000, () => console.log('listening at port 9000...'));
+app.listen(PORT, () => console.log('listening at port ' + PORT));
